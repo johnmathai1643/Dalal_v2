7a4a852f21ffd3fdc4fece2a1652fae039f41d84
Parth Thakkar <thakkarparth007@gmail.com>
2016-02-20 18:55:06 +0530

    Make auth_failure response JSON

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -2,7 +2,7 @@ module ApiHelper
 	def check_credentials!
 		u = User.find_by_email(request.headers["X-DALAL-API-EMAIL"])
 		if !u or !u.valid_password?(request.headers["X-DALAL-API-PASSWORD"])
-			render :text => "not permitted"
+			render :json => { auth_error: true }
 		else
 			@cur_user = u
 		end
